55b8aec23bc26b8a52f32e83bed0f55169c98389
hectorl2k <83291673+hectorl2k@users.noreply.github.com>
2023-02-20 16:53:10 +0100

    debuggage api exprt date start/stop

--- api/export.php
+++ api/export.php
@@ -1,99 +1,42 @@
 <?php
-include ("./../BDD/config.php");
-include ("./../BDD/requete.php");
+include("./../BDD/config.php");
+include("./../BDD/requete.php");
 
-$conn = Connection();
-
+$connection = Connection();
 $request_method = $_SERVER["REQUEST_METHOD"];
-/*
+$date = "";
 
-switch($request_method)
-{
-    case 'GET':
-        if(!empty($_GET["id"]))
-        {
-            // Récupérer un seul produit
-            $id = intval($_GET["id"]);
-            getData($id);
-        }
-        else
-        {
-            // Récupérer tous les produits
-            getData();
-        }
-        break;
-    default:
-        // Requête invalide
-        header("HTTP/1.0 405 Method Not Allowed");
-        break;
+$Max_rows = 1;
+
+if (isset($_GET['limit'])) {
+    $Max_rows = $_GET['limit'];
 }
-?>
-
-
-<?php
-function getData()
-{
-    $Max_rows=1;
-    if(isset($_GET['limit']))
-    {
-        $Max_rows=$_GET['limit'];
-    }
-    if(isset($_GET['table_select']))
-    {
-    $table =$_GET['table_select'];
-    }else    {echo "Paramétre : table_select, limit, start,stop";}
-    global $conn;
-    $date="";
-    if (isset($_GET['start'])) {
-        $date_start = $_GET['start'];
-        $date_stop = $_GET['stop'];
-
-        $date = "where (timestamp between \"$date_start\" and \"$date_stop\") ";
-    }
-    $query = "SELECT * FROM $table $date ORDER BY ID DESC LIMIT 0,$Max_rows";
-
-
-    $response = array();
-    $result = mysqli_query($conn, $query);
-    $i=0;
-    while($row = mysqli_fetch_array($result))
-    {$i++;
-    var_dump($row[1]);
-        $response[] = $row;
-    var_dump($response);
-    }
-    //var_dump($i);
-    header('Content-Type: application/json');
-    //var_dump($response);
-    //echo json_encode($response[0], JSON_PRETTY_PRINT);
-    //if
+if (isset($_GET['table_select'])) {
+    $table = $_GET['table_select'];
+} else {
+    echo "Paramétre : table_select, limit, start,stop";
 }
-?>
 
-*/
-
-//open connection to mysql db
-$connection =Connection() or die("Error " . mysqli_error($connection));
-
-$Max_rows=1;
-if(isset($_GET['limit']))
-{
-    $Max_rows=$_GET['limit'];
-}
-if(isset($_GET['table_select']))
-{
-    $table =$_GET['table_select'];
-}else    {echo "Paramétre : table_select, limit, start,stop";}
-global $conn;
-$date="";
+$code_date = 0;
 if (isset($_GET['start'])) {
+    $code_date += 1;
     $date_start = $_GET['start'];
-    $date_stop = $_GET['stop'];
-
-    $date = "where (timestamp between \"$date_start\" and \"$date_stop\") ";
 }
-$query = "SELECT * FROM $table $date ORDER BY ID DESC LIMIT 0,$Max_rows";
+if (isset($_GET['stop'])) {
+    $code_date += 10;
+    $date_stop = $_GET['stop'];
+}
+if ($code_date == 1) {
+    $date = "where (timestamp >= \"$date_start\") ";
+} elseif ($code_date == 10) {
+    $date = "where (timestamp <= \"$date_stop\") ";
+} elseif ($code_date == 11) {
+    $date = "where (timestamp >= \"$date_start\" and  timestamp <=\"$date_stop\") ";
+}
 
+
+$query = "SELECT * FROM $table $date ORDER BY ID DESC LIMIT 0,$Max_rows";
+var_dump($query);
 
 $result = mysqli_query($connection, $query) or die("Error in Selecting " . mysqli_error($connection));
 
