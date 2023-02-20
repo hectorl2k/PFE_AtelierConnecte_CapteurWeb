04908a13cc56e6a9a19a5cb9dda3cba43f499aaf
hectorl2k <83291673+hectorl2k@users.noreply.github.com>
2023-02-20 17:04:24 +0100

    Export no limit

--- api/export.php
+++ api/export.php
@@ -5,16 +5,27 @@ include("./../BDD/requete.php");
 $connection = Connection();
 $request_method = $_SERVER["REQUEST_METHOD"];
 $date = "";
-
 $Max_rows = 1;
+
+
+$erreur=0;
+$limit="LIMIT 0,$Max_rows";
 
 if (isset($_GET['limit'])) {
     $Max_rows = $_GET['limit'];
+    $limit="LIMIT 0,$Max_rows";
+    if($Max_rows<=0)
+    {
+        $limit="";
+    }
 }
+
+
 if (isset($_GET['table_select'])) {
     $table = $_GET['table_select'];
 } else {
-    echo "Paramétre : table_select, limit, start,stop";
+    echo "Paramètre(s) : table_select --> Selectionner la table , limit --> Nombre(s) de requêtes, start --> Date de début, stop --> Date de fin";
+    $erreur=1;
 }
 
 $code_date = 0;
@@ -34,20 +45,16 @@ if ($code_date == 1) {
     $date = "where (timestamp >= \"$date_start\" and  timestamp <=\"$date_stop\") ";
 }
 
-
-$query = "SELECT * FROM $table $date ORDER BY ID DESC LIMIT 0,$Max_rows";
-var_dump($query);
-
-$result = mysqli_query($connection, $query) or die("Error in Selecting " . mysqli_error($connection));
-
-//create an array
-$emparray = array();
-while ($row = mysqli_fetch_assoc($result)) {
-    $emparray[] = $row;
+if($erreur==0)
+{
+    $query = "SELECT * FROM $table $date ORDER BY ID DESC $limit";
+    $result = mysqli_query($connection, $query) or die("Error in Selecting " . mysqli_error($connection));
+    $emparray = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $emparray[] = $row;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($emparray, JSON_PRETTY_PRINT);
 }
-header('Content-Type: application/json');
-echo json_encode($emparray, JSON_PRETTY_PRINT);
-
-//close the db connection
 mysqli_close($connection);
 ?>
