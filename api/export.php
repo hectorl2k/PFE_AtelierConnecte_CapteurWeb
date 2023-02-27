adaf14623c1bc7f337e6363370aa3129c041b89d
hectorl2k <83291673+hectorl2k@users.noreply.github.com>
2023-02-27 15:55:00 +0100

    Ajout export tablename

--- api/export.php
+++ api/export.php
@@ -6,6 +6,7 @@ $connection = Connection();
 $request_method = $_SERVER["REQUEST_METHOD"];
 $date = "";
 $Max_rows = 1;
+$mode=-1;                // 1- Export TableName  2- Export Data
 
 
 $erreur=0;
@@ -21,12 +22,9 @@ if (isset($_GET['limit'])) {
 }
 
 
-if (isset($_GET['table_select'])) {
-    $table = $_GET['table_select'];
-} else {
-    echo "Paramètre(s) : table_select --> Selectionner la table , limit --> Nombre(s) de requêtes, start --> Date de début, stop --> Date de fin";
-    $erreur=1;
-}
+
+
+
 
 $code_date = 0;
 if (isset($_GET['start'])) {
@@ -45,16 +43,75 @@ if ($code_date == 1) {
     $date = "where (timestamp >= \"$date_start\" and  timestamp <=\"$date_stop\") ";
 }
 
-if($erreur==0)
+
+
+if (isset($_GET['all_boitiers'])) {
+    $mode=1;
+}
+
+
+if (isset($_GET['table_select'])) {
+    $table = $_GET['table_select'];
+    $mode=2;
+} else if($mode!=1){
+    echo "Paramètre(s) : table_select --> Selectionner la table , limit --> Nombre(s) de requêtes, start --> Date de début, stop --> Date de fin";
+    $erreur=1;
+}
+
+
+switch ($mode) {
+    case 1 :
+        ExportTableName();
+        break;
+
+    case 2 :
+        ExportData();
+        break;
+
+    default: echo "Erreur de selection de mode";
+}
+
+
+function ExportData()
 {
-    $query = "SELECT * FROM $table $date ORDER BY ID DESC $limit";
-    $result = mysqli_query($connection, $query) or die("Error in Selecting " . mysqli_error($connection));
+    global  $erreur,$table,$date,$limit,$connection;
+    if($erreur==0)
+    {
+        $query = "SELECT * FROM $table $date ORDER BY ID DESC $limit";
+        $result = mysqli_query($connection, $query) or die("Error in Selecting " . mysqli_error($connection));
+        $emparray = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $emparray[] = $row;
+        }
+        header('Content-Type: application/json');
+        echo json_encode($emparray, JSON_PRETTY_PRINT);
+    }
+
+}
+
+
+function ExportTableName()
+{
+    global $connection;
+
+
+    $result = GetTabname($connection);
     $emparray = array();
     while ($row = mysqli_fetch_assoc($result)) {
         $emparray[] = $row;
     }
+
+
+
     header('Content-Type: application/json');
     echo json_encode($emparray, JSON_PRETTY_PRINT);
+
+
 }
+
+
+
+
+
 mysqli_close($connection);
 ?>
